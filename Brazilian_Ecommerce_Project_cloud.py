0a9962c76066c2c7d892f95cac3497a8122be323
Abir.Ghali <116516902+abirElghali@users.noreply.github.com>
2023-05-11 23:54:57 +0200

    Update Brazilian_Ecommerce_Project_cloud.py

--- Brazilian_Ecommerce_Project_cloud.py
+++ Brazilian_Ecommerce_Project_cloud.py
@@ -16,31 +16,10 @@ from streamlit_pandas_profiling import st_profile_report
 model_classification = joblib.load('Brazilian Ecommerce Classification.bkl')
 model_clustering = joblib.load('Brazilian Ecommerce Clustering.bkl')
 sidebar = st.sidebar
-mode = sidebar.radio('Mode', ['Exploration de données', 'Classification', 'Clustering'])
+mode = sidebar.radio('Mode', [ 'Classification', 'Clustering'])
 st.markdown("<h1 style='text-align: center; color: #ff0000;'></h1>", unsafe_allow_html=True)
 
-if mode == "Exploration de données":
 
-    def main():
-
-        with st.sidebar.header('Upload your CSV data'):
-            uploaded_file = st.sidebar.file_uploader('Upload your input csv file')
-
-        if uploaded_file is not None:
-
-            EDA_sample = pd.read_csv(uploaded_file, index_col= 0)
-            pr = ProfileReport(EDA_sample, explorative=True)
-            st.header('**Input DataFrame**')
-            st.write(EDA_sample)
-            st.write('---')
-            st.header('**Pandas Profiling Report**')
-            st_profile_report(pr)
-        
-        else:
-            st.info('Awaiting for CSV file to be uploaded.')
-
-    if __name__ == '__main__':
-        main()
 
 if mode == "Classification":
 
